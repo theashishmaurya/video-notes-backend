fb529850547cdea1b025c13c6d3204aa2a6f3e5f
AshishIvoyant <ashish.1999vns@gmail.com>
2023-04-15 18:05:09 +0530

    Added cors enabled for *

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(8000);
 }
 bootstrap();
